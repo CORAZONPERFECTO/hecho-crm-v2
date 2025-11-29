6a8ec8bc16752d399b7ba8462deffc733d92878f
Luis <proyector27@gmail.com>
2025-11-29 01:00:32 -0400

    fix: hardcode firebase config to resolve missing env vars in vercel

--- src/integrations/firebase/client.ts
+++ src/integrations/firebase/client.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyBjuhU47D7f3tGlsh_z4INwf1pmDFvxWrs",
+    authDomain: "apphechosrl.firebaseapp.com",
+    projectId: "apphechosrl",
+    storageBucket: "apphechosrl.firebasestorage.app",
+    messagingSenderId: "63203392566",
+    appId: "1:63203392566:web:e685fc33618e273225ec97"
 };
 
 // Initialize Firebase
